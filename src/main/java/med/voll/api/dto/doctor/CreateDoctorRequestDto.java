816c08af92dd0a6287de9950f003612497a4cb3e
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-11-08 15:44:34 -0300

    Feat: add dtos

---
+++ src/main/java/med/voll/api/dto/doctor/CreateDoctorRequestDto.java
@@ -0,0 +1,23 @@
+package med.voll.api.dto.doctor;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.dto.address.DataAddress;
+import med.voll.api.entities.enums.Specialty;
+
+public record CreateDoctorRequestDto(
+        @NotBlank
+        String name,
+        @NotBlank @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Specialty specialty,
+        @NotNull @Valid
+        DataAddress address) {
+}
