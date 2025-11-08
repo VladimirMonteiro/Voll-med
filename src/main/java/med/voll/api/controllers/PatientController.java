4997b99820217c260bb74db5e7cdf88f87aae428
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-11-08 19:19:31 -0300

    Refactor: add annotation transactional

--- src/main/java/med/voll/api/controllers/PatientController.java
+++ src/main/java/med/voll/api/controllers/PatientController.java
@@ -9,6 +9,7 @@ import med.voll.api.repositories.PatientRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,22 +20,26 @@ public class PatientController {
     private final PatientRepository patientRepository;
 
     @PostMapping
+    @Transactional
     public void create (@RequestBody CreatePatientRequestDto dto) {
         patientRepository.save(new Patient(dto));
     }
 
     @GetMapping
+    @Transactional(readOnly = true)
     public Page<ListDataPatient> findAll (@PageableDefault(size = 10, sort = {"name"}) Pageable pageable) {
         return patientRepository.findAll(pageable).map(ListDataPatient::new);
     }
 
     @PutMapping
+    @Transactional
     public void update(@RequestBody UpdatePatientRequestDto dto) {
         var patient = patientRepository.getReferenceById(dto.id());
         patient.update(dto);
     }
 
     @DeleteMapping("/{id}")
+    @Transactional
     public void delete(@PathVariable Long id) {
         patientRepository.deleteById(id);
     }
