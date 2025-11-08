a231e92fa77bba25475abd90152063f90b26a5e0
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-11-08 18:54:37 -0300

    Feat: update and delete of entities

--- src/main/java/med/voll/api/entities/Address.java
+++ src/main/java/med/voll/api/entities/Address.java
@@ -1,6 +1,7 @@
 package med.voll.api.entities;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,5 +31,15 @@ public class Address {
         this.complement = address.complement();
         this.city = address.city();
         this.uf = address.uf();
+    }
+
+    public void update (@Valid DataAddress address) {
+        if (address.publicPlace() != null) this.publicPlace = address.publicPlace();
+        if (address.neighborhood() != null) this.neighborhood = address.neighborhood();
+        if (address.cep() != null) this.cep = address.cep();
+        if (address.number() != null) this.number = address.number();
+        if (address.complement() != null) this.complement = address.complement();
+        if (address.city() != null) this.city = address.city();
+        if (address.uf() != null) this.uf = address.uf();
     }
 }
--- src/main/java/med/voll/api/entities/Doctor.java
+++ src/main/java/med/voll/api/entities/Doctor.java
@@ -3,6 +3,7 @@ package med.voll.api.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import med.voll.api.dto.doctor.CreateDoctorRequestDto;
+import med.voll.api.dto.doctor.UpdateDoctorRequestDto;
 import med.voll.api.entities.enums.Specialty;
 
 @Entity
@@ -21,6 +22,7 @@ public class Doctor {
     private String email;
     private String phone;
     private String crm;
+    private boolean active;
 
     @Enumerated(EnumType.STRING)
     private Specialty specialty;
@@ -29,11 +31,22 @@ public class Doctor {
     private Address address;
 
     public Doctor(CreateDoctorRequestDto dto) {
+        this.active = true;
         this.name = dto.name();
         this.email = dto.email();
         this.phone = dto.phone();
         this.crm = dto.crm();
         this.specialty = dto.specialty();
         this.address = new Address(dto.address());
+    }
+
+    public void update (UpdateDoctorRequestDto dto) {
+        if (dto.name() != null) this.name = dto.name();
+        if (dto.phone() != null) this.phone = dto.phone();
+        if (dto.address() != null) this.address.update(dto.address());
+    }
+
+    public void delete () {
+        this.active = false;
     }
 }
