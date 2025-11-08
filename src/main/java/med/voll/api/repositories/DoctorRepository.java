e8e2914891e019ad70983dac70b313a91c17d28f
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-11-08 18:54:55 -0300

    Feat: find all active doctors

--- src/main/java/med/voll/api/repositories/DoctorRepository.java
+++ src/main/java/med/voll/api/repositories/DoctorRepository.java
@@ -1,7 +1,10 @@
 package med.voll.api.repositories;
 
 import med.voll.api.entities.Doctor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    Page<Doctor> findAllActiveTrue (Pageable pageable);
 }
