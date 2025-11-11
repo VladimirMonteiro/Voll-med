064219935590337c4eeba2d2c16074046f6bed3c
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-11-11 20:48:10 -0300

    Fix: add controller advice

---
+++ src/main/java/med/voll/api/infra/ControllerAdvice.java
@@ -0,0 +1,15 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity entityNotFoundException() {
+        return ResponseEntity.notFound().build();
+    }
+}
