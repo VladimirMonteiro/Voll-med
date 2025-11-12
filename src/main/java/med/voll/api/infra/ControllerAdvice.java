83e2d8f785a26aaddfb17319227ae0ef71aa42cf
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-11-11 21:04:54 -0300

    Feat: add validation status 400

--- src/main/java/med/voll/api/infra/ControllerAdvice.java
+++ src/main/java/med/voll/api/infra/ControllerAdvice.java
@@ -2,14 +2,32 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class ControllerAdvice {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity entityNotFoundException() {
+    public ResponseEntity<Void> entityNotFoundException() {
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorValidation>> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var errors = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ErrorValidation::new).toList());
+    }
+
+    private record ErrorValidation(String field, String message){
+        public ErrorValidation(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
 }
+
+
