98bac6409719e659600a1886932ca3e4338b6e5c
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-11-13 10:43:59 -0300

    Chore: add initial config of security

---
+++ src/main/java/med/voll/api/infra/exceptions/ControllerAdvice.java
@@ -0,0 +1,33 @@
+package med.voll.api.infra.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Void> entityNotFoundException() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorValidation>> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var errors = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ErrorValidation::new).toList());
+    }
+
+    private record ErrorValidation(String field, String message){
+        public ErrorValidation(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
+
+
