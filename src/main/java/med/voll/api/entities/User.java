77322dcd6c50e5be0e202f15033180ec5ff844c7
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-11-13 09:15:26 -0300

    Feat: added User entitiy and migration V5

---
+++ src/main/java/med/voll/api/entities/User.java
@@ -0,0 +1,29 @@
+package med.voll.api.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+    
+}
